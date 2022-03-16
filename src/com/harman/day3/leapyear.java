302a7a0a28f9716f017934c46c0f5d62463a9cf9
sumanth1231807 <sumanthravi11111@gmail.com>
2022-03-16 19:38:34 +0530

    leapyear

---
+++ src/com/harman/day3/leapyear.java
@@ -0,0 +1,17 @@
+package com.harman.day3;
+
+import java.util.Scanner;
+
+public class leapyear {
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        System.out.println("Enter Year :");
+        int year=s.nextInt();
+        String isleap=(year%4==0 && year%100!=0)?
+                "Leap Year" :
+                "Non-leap Year";
+        System.out.println(year+" is"+isleap);
+
+
+    }
+}
